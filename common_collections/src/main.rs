d07cd7a3345908d01f6f087e2095b72d96147cfa
PIO <kampio@pm.me>
2022-01-23 21:48:32 +0100

    vectors included

--- common_collections/src/main.rs
+++ common_collections/src/main.rs
@@ -1,3 +1,13 @@
+use std::collections::HashMap;
+
+
 fn main(){
-	printnl!("Hello World!);
+	println!("Hello World!");
+	let v: Vec<i32> = Vec::new();
+	let mut scores = HashMap::new();
+	scores.insert(String::from("Blue"), 10);
+	scores.insert(String::from("Yellow"), 50);
+	
+
+
 }
