4a4515659380ba11551b61c1599d624bb9f71745
PIO <kampio@pm.me>
2022-01-23 11:34:05 +0100

    message

--- enums_6/src/main.rs
+++ enums_6/src/main.rs
@@ -1,28 +1,24 @@
-enum IpAddrKind{
-    V4,
-    V6,
-}
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+enum Message{
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32,i32,i32)
 }
 
-fn main() {
-    println!("Hello, world!");
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
-
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
-
-    let home = IpAddr{
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
+impl Message{
+    fn call(&self){
+       //method body would be defined here 
+    }
 }
-fn route(ip_kind: IpAddrKind){}
+
+fn main(){
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("::1"));
+    let m = Message::Write(String::from("hello"));
+    m.call();
+}
