1118f2523379a81bd058d257db0503125904ea0d
PIO <kampio@pm.me>
2022-02-14 16:16:34 +0100

    start

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+fn main(){
+    
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+    println!("Searching for {}", query);
+    println!("In file {} ", filename); 
+
 }
